87663aeb42302cb6ca34d8d60dacc95a8e19c11c
zhp510730568 <510730568@qq.com>
2015-04-22 11:23:53 +0800

    Update Word.java

--- src/com/zhp/nlp/hashtree/Word.java
+++ src/com/zhp/nlp/hashtree/Word.java
@@ -10,50 +10,97 @@ public class Word implements Comparable<Word> {
 	private int offset = -1;
 	// 出现数量
 	private int count = 0;
-
-	public Word(String wordname) {
+	
+	public Word(String wordname, int offset, int count) {
 		this.wordName = wordname;
+		this.offset = offset;
+		this.count = count;
 	}
 	
+	/**
+	 * 获取wordName属性值
+	 * @return wordName属性值
+	 */
 	public String getWordName() {
 		return wordName;
 	}
-
+	
+	/**
+	 * 设置wordName属性值
+	 * @param wordName
+	 * @return void
+	 */
 	public void setWordName(String wordName) {
 		this.wordName = wordName;
 	}
 	
+	/**
+	 * 获取offset属性值
+	 * @return offset属性值
+	 */
 	public int getOffset() {
 		return offset;
 	}
 
+	/**
+	 * 设置offset属性值
+	 * @param offset
+	 * @return void
+	 */
 	public void setOffset(int offset) {
 		this.offset = offset;
 	}
 
+	/**
+	 * 获取offset属性值
+	 * @return offset属性值
+	 */
 	public int getCount() {
 		return count;
 	}
 
+	/**
+	 * 设置count属性值
+	 * @param count
+	 * @return void
+	 */
 	public void setCount(int count) {
 		this.count = count;
 	}
 	
+	/**
+	 * 增加count属性值
+	 * @param count
+	 * @return void
+	 */
 	public void IncreCount() {
 		this.count++;
 	}
 
+	/**
+	 * 重写比较方法
+	 * @param arg0
+	 * @return 结果值
+	 */
 	@Override
 	public int compareTo(Word arg0) {
-		if(this.wordName.equals(arg0.wordName)) {
+		if(arg0 == null) {
+			return 0;
+		}
+		if(this.wordName.equals(arg0.wordName) && this.count == arg0.count && this.offset == arg0.offset) {
 			return 1;
 		}
 		
 		return 0;
 	}
 	
+	/**
+	 * 重写clone方法
+	 * 
+	 * @return 对象副本
+	 */
 	@Override
 	public Word clone() {
-		return new Word(this.wordName);
+		return new Word(this.wordName, this.offset, this.count);
 	}
 }
